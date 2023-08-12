b6fab141366d7de2f7cf4aec02bb4a8846b90cf8
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-08-12 16:06:53 -0300

    Also create super user if none is present

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r ./requirements.txt
 cd noticias
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --username fisa --email "fisadev@gmail.com" --noinput || true
