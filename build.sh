d4cad5eb6fe7619c65b9c9ccaefdd7d08e9bf521
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-08-15 15:08:17 -0300

    Clean repo

--- build.sh
+++
@@ -1,9 +0,0 @@
-# exit on error
-set -o errexit
-
-pip install -r ./requirements.txt
-
-cd $(dirname $(find . | grep manage.py$))
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py createsuperuser --username fisa --email "fisadev@gmail.com" --noinput || true
